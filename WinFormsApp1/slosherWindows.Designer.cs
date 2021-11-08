82f43ac506e4f16acd0f0be7f16bb9d8ba7d9817
Slosher <93607233+emirslosher@users.noreply.github.com>
2021-11-08 19:55:48 +0300

    Add files via upload

---
+++ WinFormsApp1/slosherWindows.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblsayi1 = new System.Windows.Forms.Label();
+            this.lblsayi2 = new System.Windows.Forms.Label();
+            this.txtSay1 = new System.Windows.Forms.TextBox();
+            this.txtSay2 = new System.Windows.Forms.TextBox();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.btnTopla = new System.Windows.Forms.Button();
+            this.btnCikar = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnBol = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // lblsayi1
+            // 
+            this.lblsayi1.AutoSize = true;
+            this.lblsayi1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblsayi1.Location = new System.Drawing.Point(46, 37);
+            this.lblsayi1.Name = "lblsayi1";
+            this.lblsayi1.Size = new System.Drawing.Size(55, 21);
+            this.lblsayi1.TabIndex = 0;
+            this.lblsayi1.Text = "1. Sayı";
+            // 
+            // lblsayi2
+            // 
+            this.lblsayi2.AutoSize = true;
+            this.lblsayi2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblsayi2.Location = new System.Drawing.Point(46, 87);
+            this.lblsayi2.Name = "lblsayi2";
+            this.lblsayi2.Size = new System.Drawing.Size(55, 21);
+            this.lblsayi2.TabIndex = 1;
+            this.lblsayi2.Text = "2. Sayı";
+            // 
+            // txtSay1
+            // 
+            this.txtSay1.Location = new System.Drawing.Point(179, 35);
+            this.txtSay1.Name = "txtSay1";
+            this.txtSay1.Size = new System.Drawing.Size(100, 23);
+            this.txtSay1.TabIndex = 2;
+            // 
+            // txtSay2
+            // 
+            this.txtSay2.Location = new System.Drawing.Point(179, 89);
+            this.txtSay2.Name = "txtSay2";
+            this.txtSay2.Size = new System.Drawing.Size(100, 23);
+            this.txtSay2.TabIndex = 3;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblSonuc.Location = new System.Drawing.Point(372, 70);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(19, 21);
+            this.lblSonuc.TabIndex = 4;
+            this.lblSonuc.Text = "0";
+            // 
+            // btnTopla
+            // 
+            this.btnTopla.Location = new System.Drawing.Point(35, 143);
+            this.btnTopla.Name = "btnTopla";
+            this.btnTopla.Size = new System.Drawing.Size(77, 53);
+            this.btnTopla.TabIndex = 5;
+            this.btnTopla.Text = "+";
+            this.btnTopla.UseVisualStyleBackColor = true;
+            this.btnTopla.Click += new System.EventHandler(this.btnTopla_Click);
+            // 
+            // btnCikar
+            // 
+            this.btnCikar.Location = new System.Drawing.Point(141, 143);
+            this.btnCikar.Name = "btnCikar";
+            this.btnCikar.Size = new System.Drawing.Size(77, 53);
+            this.btnCikar.TabIndex = 6;
+            this.btnCikar.Text = "-";
+            this.btnCikar.UseVisualStyleBackColor = true;
+            this.btnCikar.Click += new System.EventHandler(this.btnCikar_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(258, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(77, 53);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "x";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnBol
+            // 
+            this.btnBol.Location = new System.Drawing.Point(388, 143);
+            this.btnBol.Name = "btnBol";
+            this.btnBol.Size = new System.Drawing.Size(77, 53);
+            this.btnBol.TabIndex = 8;
+            this.btnBol.Text = "/";
+            this.btnBol.UseVisualStyleBackColor = true;
+            this.btnBol.Click += new System.EventHandler(this.btnBol_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Location = new System.Drawing.Point(416, 222);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(75, 23);
+            this.btnTemizle.TabIndex = 9;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "LeFearr Best";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OrangeRed;
+            this.ClientSize = new System.Drawing.Size(518, 257);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnBol);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnCikar);
+            this.Controls.Add(this.btnTopla);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.txtSay2);
+            this.Controls.Add(this.txtSay1);
+            this.Controls.Add(this.lblsayi2);
+            this.Controls.Add(this.lblsayi1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Slosher Hesap Makinesi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblsayi1;
+        private System.Windows.Forms.Label lblsayi2;
+        private System.Windows.Forms.TextBox txtSay1;
+        private System.Windows.Forms.TextBox txtSay2;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Button btnTopla;
+        private System.Windows.Forms.Button btnCikar;
+        private System.Windows.Forms.Button btnCarp;
+        private System.Windows.Forms.Button btnBol;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WinFormsApp1/slosherWindows.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int say1, say2;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            say1 = Convert.ToInt32(txtSay1.Text);
+            say2 = Convert.ToInt32(txtSay2.Text);
+            lblSonuc.Text = ("Sonuç = " + say1 * say2).ToString();
+        }
+
+        private void btnCikar_Click(object sender, EventArgs e)
+        {
+            say1 = Convert.ToInt32(txtSay1.Text);
+            say2 = Convert.ToInt32(txtSay2.Text);
+            lblSonuc.Text = "Sonuç = " + (say1 - say2).ToString();
+        }
+
+        private void btnBol_Click(object sender, EventArgs e)
+        {
+            say1 = Convert.ToInt32(txtSay1.Text);
+            say2 = Convert.ToInt32(txtSay2.Text);
+            lblSonuc.Text = ("Sonuç = "+ say1 / say2).ToString();
+        }
+
+
+        private void btnTopla_Click(object sender, EventArgs e)
+        {
+            say1 = Convert.ToInt32(txtSay1.Text);
+            say2 = Convert.ToInt32(txtSay2.Text);
+            lblSonuc.Text =  ("Sonuç = ") + (say1 + say2).ToString();
+
+        }
+
+
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            this.Text = "Hesap Makinesi";
+            lblSonuc.Text = "0";
+
+        }
+    }
+}
